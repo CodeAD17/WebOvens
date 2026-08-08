c9a62cbc27c9b30645138c886f7aeec17e8b406c
WebOvens <webovens.in@gmail.com>
2026-08-08 18:45:59 +0530

    site map

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://webovens.agency/sitemap.xml',
+    sitemap: 'https://webovens.in/sitemap.xml',
   };
 }
